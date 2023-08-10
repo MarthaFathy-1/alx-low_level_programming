1b65d0460601b8ce1fbe139f72f00c25a8a81480
MarthaFathy-1 <fathimartha@gmail.com>
2023-08-10 09:59:03 -0700

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+/**
+ * main - the main function
+ *
+ * Return: Always return (0)
+ */
+int main(void)
+{
+	char str[]= "\"Programming is like building a multilingual puzzle\n";
+	puts(str);
+	return (0);
+}
