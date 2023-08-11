45b5bb7b9683351d40c15fd2ee0c75f05789bf52
MarthaFathy-1 <fathimartha@gmail.com>
2023-08-11 07:10:22 -0700

    3-print_alphabets.c

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ *
+ * Return: return value always (0)
+ */
+int main(void)
+{
+        char lowercase[] = {'a', 'b', 'c', 'd', 'e', 'f', 'g',
+        'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q',
+         'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'};
+        int i;
+
+        for (i = 0; i <= lowercase[i]; i++)
+        {
+                putchar(lowercase[i]);
+        }
+	for (i = 0; i <= lowercase[i]; i++)
+	{
+		putchar(lowercase[i]-=32);
+	}
+        putchar('\n');
+        return (0);
+}
