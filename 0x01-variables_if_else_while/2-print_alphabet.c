7e51daada7fde9a8ae5b6a185579a0560640bd64
MarthaFathy-1 <fathimartha@gmail.com>
2023-08-11 07:02:07 -0700

    2-print_alphabet.c

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -6,11 +6,12 @@
  */
 int main(void)
 {
-	char lowercase[] = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q',
+	char lowercase[] = {'a', 'b', 'c', 'd', 'e', 'f', 'g',
+	'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q',
 	 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'};
 	int i;
 
-	for (i = 0; i < lowercase[i]; i++)
+	for (i = 0; i <= lowercase[i]; i++)
 	{
 		putchar(lowercase[i]);
 	}
