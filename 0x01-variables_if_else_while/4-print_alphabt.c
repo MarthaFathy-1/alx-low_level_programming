df448f5bc311a8e074081f9d9eb5e59792364992
MarthaFathy-1 <fathimartha@gmail.com>
2023-08-11 07:27:06 -0700

    4-print_alphabt.c

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: return value
+ */
+int main(void)
+{
+	char lowercase[] = {'a', 'b', 'c', 'd', 'f', 'g',
+		'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p',
+		'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'};
+	int i;
+
+	for(i = 0; i <= lowercase[i]; i++)
+	{
+		putchar(lowercase[i]);
+	}
+	putchar('\n');
+	return (0);
+}
