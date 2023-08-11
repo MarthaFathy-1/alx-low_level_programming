0b0e2b7f3d035a95ea0df4714141be1846bb3ca8
MarthaFathy-1 <fathimartha@gmail.com>
2023-08-11 13:55:45 -0700

    9-print_comb.c

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -8,11 +8,14 @@ int main(void)
 {
 	int d;
 
-	for (d = '0'; d <= '9'; d++)
+	for (d = 0; d <= 9; d++)
 	{
-		putchar(d);
-		putchar(',');
-		putchar(' ');
+		putchar(d + '0');
+		if (d < 9)
+		{
+			putchar(',');
+			putchar(' ');
+		}
 	}
 	putchar('\n');
 	return (0);
