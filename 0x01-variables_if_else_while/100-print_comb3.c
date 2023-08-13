3826ab59daacc2005def3d3dbc9c8411cd794c18
MarthaFathy-1 <fathimartha@gmail.com>
2023-08-13 09:11:30 -0700

    100-print_comb3.c

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: return value 0
+ */
+int main(void)
+{
+	int r, m;
+	for(r = 0; r < 10; r++)
+	{
+		for(m = r+1; m < 10; m++)
+		{
+			putchar(r + '0');
+			putchar(m + '0');
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+	return (0);
+}
