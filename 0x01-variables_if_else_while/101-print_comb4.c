c6425de90ab8a01d61354a6bc08915f7e2296eed
MarthaFathy-1 <fathimartha@gmail.com>
2023-08-14 01:14:14 -0700

    101-print_comb4.c

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Description: program that prints all possible different combinations of three digits
+ * Return: return value 0
+ */
+int main(void)
+{
+	int r, q, l;
+
+	for (r = 0; r < 10; r++)
+	{
+		for (q = r + 1; q < 10; q++)
+		{
+			for (l = q + 1; l < 10; l++)
+			{
+				putchar(r + '0');
+				putchar(q + '0');
+				putchar(l+ '0');
+				if (r + q + l != 24)
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
