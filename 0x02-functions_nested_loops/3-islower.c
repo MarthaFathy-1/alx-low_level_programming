be9a5512f826f235fccacd392ac0eee53463b43f
MarthaFathy-1 <fathimartha@gmail.com>
2023-08-15 10:13:49 -0700

    3-islower.c

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * _islower - function that checks for lowercase character
+ *
+ * Return: Always 0
+ */
+int _islower(int c)
+{
+	int r;
+
+	if (_islower(r) == 1)
+	{
+		_putchat('1');
+	}
+	else
+	{
+		_putchar('0');
+	}
+	return (0);
+}
