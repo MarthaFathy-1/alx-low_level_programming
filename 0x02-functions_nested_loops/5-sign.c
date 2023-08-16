1c8da6b40adb1adfe764cab386cced4f0a718d80
MarthaFathy-1 <fathimartha@gmail.com>
2023-08-15 17:15:16 -0700

    5-sign.c

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -9,21 +9,21 @@ int print_sign(int n)
 {
 	if (n > 0)
 	{
-		_putchar('n');
+		_putchar(n);
 		_putchar(',');
 		_putchar(' ');
 		return (1);
 	}
 	else if ( n == 0)
 	{
-	       	_putchar('n');
+	       	_putchar(n);
                 _putchar(',');
                 _putchar(' ');
                 return (0);
 	}
 	else
 	{
-		_putchar('n');
+		_putchar(n);
                 _putchar(',');
                 _putchar(' ');
                 return (-1);
