75919b5982945d52b4b856e8991d30d7658b0fcd
MarthaFathy-1 <fathimartha@gmail.com>
2023-08-15 19:15:33 -0700

    11-print_to_98.c

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -13,10 +13,11 @@ void print_to_98(int n)
 	for (i = n; i <= 98; i++)
 	{
 		printf("%d", i);
-		if (i != 98)
+		while (i < 98)
 		{
 			_putchar(',');
 			_putchar(' ');
+			i++;
 		}
 	}
 	printf("\n");
