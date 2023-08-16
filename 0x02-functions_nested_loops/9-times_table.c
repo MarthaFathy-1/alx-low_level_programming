63bf92d3cbd17a715c1478d452d39d5e1cc19359
MarthaFathy-1 <fathimartha@gmail.com>
2023-08-15 18:42:44 -0700

    9-times_table.c

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * times_table - function that prints the 9 times table
+ *
+ * Return: no value
+ */
+void times_table(void)
+{
+	int i, j;
+
+	for (i = 0; i <= 9; i++)
+	{
+		for (j = 0; j <= 9; j++)
+		{
+			_putchar(i * j);
+			if (i + j != 18)
+			{
+			_putchar(',');
+			_putchar('..');
+			}
+		}
+		_putchar('\n');
+	}
