7056473572367f8c96e9d29f641fac8f2d27aad4
MarthaFathy-1 <fathimartha@gmail.com>
2023-08-15 18:58:37 -0700

    10-add.c

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -2,11 +2,14 @@
 /**
  * add - function that adds two integers 
  *
+ * @m : num1
+ * @t : num2
  * Return: sum of two numbers
  */
 int add(int m, int t)
 {
 	int result;
+
 	result = m + t;
 	return (result);
 }
