3daa3c3762eb3081edd1973c49953cf5a4223586
MarthaFathy-1 <fathimartha@gmail.com>
2023-08-17 09:17:33 -0700

    4-print_most_numbers.c

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -7,13 +7,13 @@
  */
 void print_most_numbers(void)
 {
-	char i;
+	int i;
 
-	for (i = '0'; i <= '9'; i++)
+	for (i = 0; i <= 9; i++)
 	{
-		if (i != '2' || i != '4')
+		if (i != 2 || i != 4)
 		{
-			_putchar(i);
+			_putchar(i + 48);
 		}
 	}
 	_putchar('\n');
