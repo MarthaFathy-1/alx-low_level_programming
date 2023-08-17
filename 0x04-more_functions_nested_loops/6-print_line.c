68c7d3bd3daebc08259ebded7ca62209e33743b2
MarthaFathy-1 <fathimartha@gmail.com>
2023-08-17 09:44:38 -0700

    6-print_line.c

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * print_line - function that draws a straight line
+ *
+ * Return: void
+ */
+void print_line(int n)
+{
+	int b;
+
+	if (n <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		for (b = 0; b < n; b++)
+		{
+			_putchar('_');
+		}
+		_putchar('\n');
+	}
+}
