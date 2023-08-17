38b7fa39aa070e96b8e8ecbd1d0c39422e04bbd7
MarthaFathy-1 <fathimartha@gmail.com>
2023-08-17 04:53:51 -0700

    4-print_most_numbers.c

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * print_most_numbers - function that prints the numbers, from 0 to 9
+ *
+ * Return: void
+ */
+void print_most_numbers(void)
+{
+	int i;
+
+	for (i = 0; i <= 9; i++)
+	{
+		if (i != 2 || i != 4)
+		{
+			printf("%d", i);
+		}
+	}
+	printf("\n");
+}
