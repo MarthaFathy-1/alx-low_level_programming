d2849d2dfcd6bde558acf8d1503c447add252519
MarthaFathy-1 <fathimartha@gmail.com>
2023-08-18 00:23:09 -0700

    7-print_diagonal.c

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,28 @@
+#include "main.h"
+/**
+ * print_diagonal -  function that draws a diagonal line
+ *
+ * @n: number of rows
+ * Return: void
+ */
+void print_diagonal(int n)
+{
+	int r, c;
+
+	if (n <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		for (r = 1; r <= n; r++)
+		{
+			for (c = 1; c <= n; c++)
+			{
+				_putchar(' ');
+			}
+			_putchar('\');
+			_putchar('\n');
+		}
+	}
+}
---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ *
+ *
+ *
+ */
+int main()
+{
+	int i, k;
+	char ch[i];
+
+	for (i = 1; i <= 100; i++)
+	{
+		int t, f, b;
+		t = i / 3;
+		f = i / 5;
+		while (t == 0 || f == 0)
+
