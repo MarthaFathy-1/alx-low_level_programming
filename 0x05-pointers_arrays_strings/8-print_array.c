941643895d1981eff1185bdbe86cb8a029582fe8
MarthaFathy-1 <fathimartha@gmail.com>
2023-08-22 10:57:53 -0700

    n

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -13,9 +13,13 @@ void print_array(int *a, int n)
 
 	for (k = 0; k < n; k++)
 	{
-		if (a[k] != '\0')
+		if (k != (n - 1))
 		{
 			printf("%d, ", a[k]);
+		}
+		else
+		{
+			printf("%d", a[k]);
 		}
 	}
 }
